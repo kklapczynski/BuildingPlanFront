908684a76b40abf9e76e917390bc9695f8b363e5
kk <k.klapczynski@gmail.com>
2020-01-29 14:33:59 +0100

    Displays image when API returns jpg, but breaks when svg: sanitization of Angular stops it.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpService } from './services/http.service';
+import { DOMPurify } from 'dompurify';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'BuildingPlanFront';
+  imageUrl = 'http://localhost:8001/testImage';
+  imageToShow: any;
+  isImageLoaded: boolean;
+
+  constructor(private httpService: HttpService) {}
+
+  ngOnInit() {
+    this.isImageLoaded = false;
+  }
+
+  getImage() {
+    this.httpService.getImage(this.imageUrl).subscribe(
+      imageBlob => {
+        console.log(imageBlob);
+        this.createImageFromBlob(imageBlob);
+      },
+      error => {
+        console.log(error);
+      }
+    )
+  }
+
+  createImageFromBlob(image: Blob) {
+    // js file reader :  https://developer.mozilla.org/en-US/docs/Web/API/FileReader
+
+    let reader = new FileReader();
+    // set listener: when loaded save result in property
+    reader.onload = () => { 
+      this.imageToShow = reader.result;
+      this.isImageLoaded = true;
+    }
+    // load blob as data url , which can be used as src form img in html
+    // reader.readAsDataURL(image);
+    DOMPurify.sanitize(reader.readAsDataURL(image));
+  }
+  
 }
---
+++ src/app/services/http.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(private http: HttpClient) { }
+
+  // getCalendars() {
+  //   // returns error: Failed to load resource: the server responded with a status of 401 ()
+  //   // cannot register app with microsoft (admin setting for capgemini ms account)
+  //   this.http.get('https://outlook.office.com/api/v2.0/me/calendars').subscribe(
+  //     response => {
+  //       console.log(response);
+  //       this.dataFromOutlook = response;
+  //     }
+  //   )
+  // }
+
+  getImage(imageUrl: string): Observable<Blob> {
+    return this.http.get(imageUrl, {responseType: 'blob'});
+  }
+  
+}
